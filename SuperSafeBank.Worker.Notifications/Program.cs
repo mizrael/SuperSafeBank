e480756e57f410d0df476c30a1fbffe6af20b460
Davide Guida <mizrael@yahoo.it>
2020-06-12 00:20:27 -0400

    added Serilog + Loki

--- SuperSafeBank.Worker.Notifications/Program.cs
+++ SuperSafeBank.Worker.Notifications/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.Loki;
 using SuperSafeBank.Core;
 using SuperSafeBank.Core.EventBus;
 using SuperSafeBank.Domain;
@@ -21,10 +25,28 @@ namespace SuperSafeBank.Worker.Notifications
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(configurationBuilder => {
+                    configurationBuilder.AddCommandLine(args);
+                })
                 .ConfigureAppConfiguration((ctx, builder) =>
                 {
                     builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                        .AddJsonFile($"appsettings.{ctx.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                        .AddJsonFile($"appsettings.{ctx.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddEnvironmentVariables();
+                })
+                .UseSerilog((ctx, cfg) =>
+                {
+                    var credentials = new NoAuthCredentials(ctx.Configuration.GetConnectionString("loki"));
+                    var labels = new Dictionary<string, string>()
+                    {
+                        {"app", ctx.HostingEnvironment.ApplicationName},
+                        {"envName", ctx.HostingEnvironment.EnvironmentName}
+                    };
+
+                    cfg.MinimumLevel.Verbose()
+                        .Enrich.FromLogContext()
+                        .WriteTo.Console(new RenderedCompactJsonFormatter())
+                        .WriteTo.LokiHttp(credentials, new LogLabelProvider(labels));
                 })
                 .ConfigureServices((hostContext, services) =>
                 {
