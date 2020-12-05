a8a24204ee947880edda9bee521e2099f3cd0dad
David Guida <Davide.Guida@equisoft.com>
2020-12-04 21:25:50 -0500

    fixed mapping issue
    MongoDB.Bson.BsonSerializationException: 'Creator map for class SuperSafeBank.Web.Core.Queries.Models.CustomerArchiveItem has 4 arguments, but none are configured" at line 22 in file QueryDbContext.cs in project SuperSafeBank.Web.Persistence.Mongo.

--- SuperSafeBank.Web.Persistence.Mongo/QueryDbContext.cs
+++ SuperSafeBank.Web.Persistence.Mongo/QueryDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using SuperSafeBank.Web.Core.Queries.Models;
 
@@ -12,14 +13,43 @@ namespace SuperSafeBank.Web.Persistence.Mongo
         {
             _db = db;
 
-            var collectionSettings = new MongoCollectionSettings
-            {
-                GuidRepresentation = GuidRepresentation.Standard
-            };
+            if (!BsonClassMap.IsClassMapRegistered(typeof(AccountDetails)))
+                BsonClassMap.RegisterClassMap<AccountDetails>(mapper=>
+                {
+                    mapper.MapProperty(c => c.Balance);
+                    mapper.MapProperty(c => c.Id);
+                    mapper.MapProperty(c => c.OwnerFirstName);
+                    mapper.MapProperty(c => c.OwnerId);
+                    mapper.MapProperty(c => c.OwnerLastName);
+                    mapper.MapProperty(c => c.Version);
+                    mapper.MapCreator(c => new AccountDetails(c.Id, c.OwnerId, c.OwnerFirstName, c.OwnerLastName, c.Balance, c.Version));
+                });
 
-            AccountsDetails = _db.GetCollection<AccountDetails>("accounts", collectionSettings);
-            CustomersDetails = _db.GetCollection<CustomerDetails>("customerdetails", collectionSettings);
-            Customers = _db.GetCollection<CustomerArchiveItem>("customers", collectionSettings);
+            if (!BsonClassMap.IsClassMapRegistered(typeof(CustomerDetails)))
+                BsonClassMap.RegisterClassMap<CustomerDetails>(mapper =>
+            {
+                mapper.MapProperty(c => c.Id);
+                mapper.MapProperty(c => c.Firstname);
+                mapper.MapProperty(c => c.Lastname);
+                mapper.MapProperty(c => c.Email);
+                mapper.MapProperty(c => c.Accounts);
+                mapper.MapProperty(c => c.TotalBalance);
+                mapper.MapCreator(c => new CustomerDetails(c.Id, c.Firstname, c.Lastname, c.Email, c.Accounts, c.TotalBalance));
+            });
+
+            if (!BsonClassMap.IsClassMapRegistered(typeof(CustomerArchiveItem)))
+                BsonClassMap.RegisterClassMap<CustomerArchiveItem>(mapper =>
+            {
+                mapper.MapProperty(c => c.Id);
+                mapper.MapProperty(c => c.Firstname);
+                mapper.MapProperty(c => c.Lastname);
+                mapper.MapProperty(c => c.Accounts);
+                mapper.MapCreator(c => new CustomerArchiveItem(c.Id, c.Firstname, c.Lastname, c.Accounts));
+            });
+
+            AccountsDetails = _db.GetCollection<AccountDetails>("accounts");
+            CustomersDetails = _db.GetCollection<CustomerDetails>("customerdetails");
+            Customers = _db.GetCollection<CustomerArchiveItem>("customers");
         }
 
         public IMongoCollection<AccountDetails> AccountsDetails { get; }
